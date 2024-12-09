d06ae6fd8f953bb8216d7f334a35c585cdf313f2
j.medeiros <j.medeiros@rioanalytics.com.br>
2024-12-09 10:46:50 -0300

    fix illustration

--- src/illustrate.py
+++ src/illustrate.py
@@ -15,16 +15,31 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 def generate_image(prompt):
     try:
         # Call the image generation API
-        response = openai.Image.create(
+        response = openai.images.generate(
             prompt=prompt,
             n=1,  # number of images to generate
             size="1024x1024"  # image size; modify as needed
         )
 
         # Extract the URL of the generated image
-        image_url = response['data'][0]['url']
+        image_url = response.data[0].url
         return image_url
 
     except Exception as e:
         return f"An error occurred: {e}"
     
+
+
+def get_illustration_per_stage(writing_outputs):
+
+    story_stages = list(writing_outputs.keys()).copy()
+    story_stages.remove('story_idea')
+
+    img_url_per_stage = {}
+
+    for stage in story_stages:
+
+        img_url = generate_image(stage)
+        img_url_per_stage[stage] = img_url
+
+    return img_url_per_stage
